6630d92890c6f646754be1e210d9385056f7320f
justitsi <hristob1999@gmail.com>
2022-06-29 11:33:28 +0300

    Add support for managed user registration * Add /users/register route to support user registration by admins in managed environments

--- src/users/users.py
+++ src/users/users.py
@@ -1,16 +1,55 @@
+from email.policy import default
 from flask import Blueprint, request
 from models.models import User, Role, db
 from util import generateError, generateResponse, validateJWT, userIsAdmin
 
 users_blueprint = Blueprint('users', __name__)
 
-@users_blueprint.route('/unregister', methods=['POST'])
-def unregisterUsers ():
+
+@users_blueprint.route('/register', methods=['POST'])
+def registerUsers():
     try:
-        valid = validateJWT(request) 
+        valid = validateJWT(request)
     except:
         return generateError(500, "Could not validate jwt_token")
-    
+
+    if (not valid):
+        return generateError(400, "Invalid jwt_token")
+    if (not userIsAdmin(valid["userID"])):
+        return generateError(403, "User is not admin")
+
+    try:
+        usersToAdd = request.json["users"]
+        registeredUsers = []
+
+        defaultUserRoles = [Role.query.filter_by(name='user').first()]
+
+        for userIDToAdd in usersToAdd:
+            # check if user already exists
+            userEntry = User.query.filter_by(id=userIDToAdd).first()
+            if not userEntry:
+                userEntry = User(
+                    id=userIDToAdd,
+                    roles=defaultUserRoles
+                )
+                db.session.add(userEntry)
+                registeredUsers.append(userIDToAdd)
+
+        db.session.commit()
+        return generateResponse({
+            "users": registeredUsers
+        })
+    except:
+        return generateError(500, "Could not proccess request")
+
+
+@users_blueprint.route('/unregister', methods=['POST'])
+def unregisterUsers():
+    try:
+        valid = validateJWT(request)
+    except:
+        return generateError(500, "Could not validate jwt_token")
+
     if (not valid):
         return generateError(400, "Invalid jwt_token")
     if (not userIsAdmin(valid["userID"])):
@@ -26,32 +65,33 @@ def unregisterUsers ():
                 if (user_entry):
                     unregistered_users.append(user_entry.id)
                     db.session.delete(user_entry)
-                    
+
         db.session.commit()
-        
+
         return generateResponse({
-                    "users": unregistered_users
-                })
+            "users": unregistered_users
+        })
     except:
         return generateError(500, "Could not proccess request")
 
+
 @users_blueprint.route('/bind/<role_id>', methods=['POST'])
-def bindRoleToUsers (role_id):
+def bindRoleToUsers(role_id):
     try:
-        valid = validateJWT(request) 
+        valid = validateJWT(request)
     except:
         return generateError(500, "Could not validate jwt_token")
-    
+
     if (not valid):
         return generateError(400, "Invalid jwt_token")
     if (not userIsAdmin(valid["userID"])):
         return generateError(403, "User is not admin")
-    
+
     try:
         role_id = int(role_id)
         role = Role.query.filter_by(id=role_id).first()
         if (not role):
-            return generateError (404, 'Role not found')
+            return generateError(404, 'Role not found')
 
         action = request.json["action"]
         users_to_add = request.json["users"]
@@ -68,44 +108,44 @@ def bindRoleToUsers (role_id):
 
         # figure out user information
         for user_id in users_to_add:
-                user_db_entry = User.query.filter_by(id = user_id).first()
-                if (user_db_entry):
-                    # check if role is already set for that user
-                    role_set = False
-                    for user_role in user_db_entry.roles:
-                        if (user_role.id == role_id):
-                            role_set = True
-                    # handle adding or removing role for user
-                    if (action == "add"):
-                        if (role_set == False):
-                            user_db_entry.roles.append(role)
-                            db.session.add(user_db_entry)
-                            users_added.append(user_db_entry.id)
-                    
-                    if (action == "remove"):
-                        if (role_set == True):
-                            user_db_entry.roles.remove(role)
-                            db.session.add(user_db_entry)
-                            users_added.append(user_db_entry.id)
+            user_db_entry = User.query.filter_by(id=user_id).first()
+            if (user_db_entry):
+                # check if role is already set for that user
+                role_set = False
+                for user_role in user_db_entry.roles:
+                    if (user_role.id == role_id):
+                        role_set = True
+                # handle adding or removing role for user
+                if (action == "add"):
+                    if (role_set == False):
+                        user_db_entry.roles.append(role)
+                        db.session.add(user_db_entry)
+                        users_added.append(user_db_entry.id)
+
+                if (action == "remove"):
+                    if (role_set == True):
+                        user_db_entry.roles.remove(role)
+                        db.session.add(user_db_entry)
+                        users_added.append(user_db_entry.id)
 
         db.session.commit()
-        
+
         return generateResponse({
-                "role": Role.jsonify(role),
-                "action": action,
-                "users": users_added
-            })
+            "role": Role.jsonify(role),
+            "action": action,
+            "users": users_added
+        })
     except:
         return generateError(500, "Could not proccess request")
 
 
 @users_blueprint.route('/byRole/<role_id>', methods=['GET'])
-def getUsersByRole (role_id):
+def getUsersByRole(role_id):
     try:
-        valid = validateJWT(request) 
+        valid = validateJWT(request)
     except:
         return generateError(500, "Could not validate jwt_token")
-    
+
     if (not valid):
         return generateError(400, "Invalid jwt_token")
     if (not userIsAdmin(valid["userID"])):
@@ -117,7 +157,7 @@ def getUsersByRole (role_id):
 
         if (not role):
             return generateError(404, "Role not found")
-        
+
         users_db = User.query.filter(User.roles.any(id=role.id)).all()
         users_list = []
         for user in users_db:
@@ -132,17 +172,17 @@ def getUsersByRole (role_id):
 
 
 @users_blueprint.route('/all/<page_size>/<page_num>', methods=['GET'])
-def getUsersList (page_size, page_num):
+def getUsersList(page_size, page_num):
     try:
-        valid = validateJWT(request) 
+        valid = validateJWT(request)
     except:
         return generateError(500, "Could not validate jwt_token")
-    
+
     if (not valid):
         return generateError(400, "Invalid jwt_token")
     if (not userIsAdmin(valid["userID"])):
         return generateError(403, "User is not admin")
-    
+
     try:
         # clean up paramaters
         page_size = int(page_size)
@@ -156,7 +196,6 @@ def getUsersList (page_size, page_num):
         users_db = User.query.all()
         paged_results = list(paginateArray(users_db, page_size))
         users_db = paged_results[page_num]
-
 
         # format data and return
         users_json = []
